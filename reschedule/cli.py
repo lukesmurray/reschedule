b1e8ad531f16c62b5597ad79205cada7cc0707af
Luke Murray <lukepigeonmail@gmail.com>
2020-04-01 10:10:50 -0400

    formatting and started typer integration

--- reschedule/cli.py
+++ reschedule/cli.py
@@ -3,8 +3,13 @@ from __future__ import print_function
 from .config import task_file_path
 from .task_parser import TaskParser
 from .scheduler import schedule_tasks
+import typer
 
 
+app = typer.Typer()
+
+
+@app.command()
 def run():
     """Run the scheduler.
 
@@ -12,3 +17,25 @@ def run():
     """
     tasks = TaskParser().get_tasks(task_file_path)
     schedule_tasks(tasks)
+
+
+@app.command()
+def config():
+    """View, update, or edit the reschedule config file.
+    """
+    typer.echo("not implemented yet")
+
+
+@app.command()
+def login():
+    """Login or change google calendar accounts.
+    """
+    typer.echo("not implemented yet")
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
